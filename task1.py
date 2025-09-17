64e5226d4f87131f40ebf35ab507ee4308330678
arpanchauhan1608-lang <arpan.chauhan1608@gmail.com>
2025-09-17 14:30:30 +0530

    initail commit

---
+++ task1.py
@@ -0,0 +1,12 @@
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+
+addition = n1 + n2
+subtraction = n1 - n2
+multiplication = n1 * n2
+division = n1 / n2 
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
---
+++ task2.py
@@ -0,0 +1,6 @@
+fn = input("Enter your first name: ")
+ln = input("Enter your last name: ")
+
+full_name = fn + " " + ln
+
+print(f"Hello, {full_name}! Welcome to the Python world!")
